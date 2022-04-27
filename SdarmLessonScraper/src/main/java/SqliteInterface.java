8d7401ea20f13d565603191f8ba8e32257101e1e
Simkortet <sim465vman@live.dk>
2022-04-27 20:34:13 +0200

    .

---
+++ SdarmLessonScraper/src/main/java/SqliteInterface.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SqliteInterface {
+    private final static String connectionString = "/home/simon/Development/SqliteDB";
+
+    public static void executeStatement(String userStatement){
+        Connection connection = null;
+        try
+        {
+            connection = DriverManager.getConnection(connectionString);
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+            statement.executeUpdate(userStatement);
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        finally
+        {
+            try
+            {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
