2a77b27053b03bafaaa88f964735aaa053e0a0cf
EsbenNedergaard <esben.nedergaard@yahoo.dk>
2022-05-02 09:54:57 +0200

    config.properties added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import config.ConfigProperties;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+
+        String connectionString = ConfigProperties.INSTANCE.getConnectionString();
+        System.out.println(connectionString);
     }
 }
---
+++ src/main/java/config/ConfigProperties.java
@@ -0,0 +1,24 @@
+package config;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigProperties extends Properties {
+    public static final ConfigProperties INSTANCE = new ConfigProperties("src/main/resources/config.properties");
+
+    private ConfigProperties(String path) {
+        File file = new File(path);
+        try {
+            FileReader fileReader = new FileReader(file);
+            load(fileReader);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getConnectionString() {
+        return getProperty("database.connection_string");
+    }
+}
