d9fbe600b4c80ad4e995070bfaa37d356259ddcb
EsbenNedergaard <esben.nedergaard@yahoo.dk>
2022-05-02 10:03:49 +0200

    Possible structure of DB connections

---
+++ src/main/java/housing/storage/HousingDataBaseConnection.java
@@ -0,0 +1,12 @@
+package housing.storage;
+
+import storage.H2Connection;
+
+public class HousingDataBaseConnection extends H2Connection {
+
+    // TODO: implement specific methods in here i.e. insert statements for:
+        // Houses
+        // Prices
+
+
+}
---
+++ src/main/java/storage/DataBaseConnection.java
@@ -0,0 +1,5 @@
+package storage;
+
+public interface DataBaseConnection {
+    // TODO: consider if we want to have a general interface as it might make it easier to support other connection types
+}
