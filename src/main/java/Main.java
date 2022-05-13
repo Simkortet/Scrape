06940708807c6a362ded0eff0d05d09e23df0486
Simkortet <sim465vman@live.dk>
2022-05-13 21:05:51 +0200

    Added config parameter support for Sdarm lesson range

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import config.ConfigProperties;
 import housing.storage.HousingDataBaseConnection;
+import sdarm.scrape.SdarmScraper;
 import storage.SQLiteConnection;
 
 import java.sql.Connection;
@@ -8,22 +9,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-
-
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-        esbenMain();
-        //simonMain();
+        switch (args[0]) {
+            case "sdarm" -> sdarmScrape();
+            case "housing" -> housingScrape();
+            default -> System.out.println("Scraper for: " + args[0] + " does not exist");
+        }
     }
 
-
-    private static void esbenMain() {
+    private static void housingScrape() {
         SQLiteConnection connection = new HousingDataBaseConnection(ConfigProperties.INSTANCE.getHousingConnectionString());
 
         connection.close();
     }
 
-    private static void simonMain() {
-
+    private static void sdarmScrape() {
+        SdarmScraper.start(ConfigProperties.INSTANCE.getSdarmLessonScrapeRange());
     }
 }
--- src/main/java/config/ConfigProperties.java
+++ src/main/java/config/ConfigProperties.java
@@ -1,9 +1,15 @@
 package config;
 
+import sdarm.records.SdarmLessonScrapeRange;
+import sdarm.records.SdarmLessonYearQuarterNumber;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 public class ConfigProperties extends Properties {
     public static final ConfigProperties INSTANCE = new ConfigProperties("src/main/resources/config.properties");
@@ -20,5 +26,23 @@ public class ConfigProperties extends Properties {
 
     public String getHousingConnectionString() {
         return getProperty("database.housing_connection_string");
+    }
+
+    public SdarmLessonScrapeRange getSdarmLessonScrapeRange() {
+        List<Integer> inputRangeParameters = Arrays.stream(getProperty("sdarm_lesson_interval_to_scrape")
+                .split(","))
+                .map(Integer::parseInt)
+                .toList();
+
+        return new SdarmLessonScrapeRange(
+                new SdarmLessonYearQuarterNumber(
+                        inputRangeParameters.get(0),
+                        inputRangeParameters.get(1),
+                        inputRangeParameters.get(2)),
+                new SdarmLessonYearQuarterNumber(
+                        inputRangeParameters.get(3),
+                        inputRangeParameters.get(4),
+                        inputRangeParameters.get(5)
+                ));
     }
 }
