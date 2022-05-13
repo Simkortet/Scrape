347efb9d599992b83f9c622175125c58b4080a06
Simkortet <sim465vman@live.dk>
2022-05-13 21:06:38 +0200

    Implemented jsoup fetch to SdarmLessons

--- src/main/java/sdarm/scrape/SdarmScraper.java
+++ src/main/java/sdarm/scrape/SdarmScraper.java
@@ -1,6 +1,9 @@
 package sdarm.scrape;
 
-import sdarm.records.meta.SdarmLessonDate;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import sdarm.records.SdarmLessonScrapeRange;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,16 +12,28 @@ public class SdarmScraper {
 
     private static final String baseUrlEng = "https://sdarm.org/publications/periodicals/sbl/en/";
 
-    public static void start(){
+    public static void start(SdarmLessonScrapeRange lessonRange){
+        List<String> urlsToScrape = getUrlsToScrape(lessonRange);
+        List<Document> lessonNodes = getSdarmDocuments(urlsToScrape);
+
 
     }
 
-    private static List<String> getUrlsToScrape(int startLessonNumber, int startLessonYear, int endLessonNumber, int endLessonYear){
+    private static List<Document> getSdarmDocuments(List<String> urlsToScrape) {
+        List<Document> sdarmDocuments = new ArrayList<>();
+
+        for (String url : urlsToScrape){
+            sdarmDocuments.add(Jsoup.parse(url));
+        }
+        return sdarmDocuments;
+    }
+
+    private static List<String> getUrlsToScrape(SdarmLessonScrapeRange lessonRange){
         List<String> urlsToScrape = new ArrayList<>();
 
-        for (int year = startLessonYear; year < endLessonYear; year++){
-            for (int quarter = 1; quarter <= 4; quarter++){
-                for (int lessonNumber = startLessonNumber; lessonNumber <= endLessonNumber; lessonNumber++){
+        for (int year = lessonRange.startLesson().year(); year < lessonRange.endLesson().year(); year++){
+            for (int quarter = lessonRange.startLesson().quarter(); quarter <= lessonRange.endLesson().quarter(); quarter++){
+                for (int lessonNumber = lessonRange.startLesson().number(); lessonNumber <= lessonRange.endLesson().number(); lessonNumber++){
                     urlsToScrape.add(baseUrlEng + year + "/" + quarter + "/" + lessonNumber + "/");
                 }
             }
